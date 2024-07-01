94d38be82ba28fa4bde8402217a76c24543c8549
Ritger Teunissen <ritger.teunissen@alliander.com>
2024-07-01 11:47:09 +0200

    Update to Netbeheer Nederland style. Remove unused files.

--- src/im_e_state_variables/datamodel/__init__.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-from .im_e_state_variables import *
-
-THIS_PATH = Path(__file__).parent
-
-SCHEMA_DIRECTORY = THIS_PATH.parent / "schema"
-MAIN_SCHEMA_PATH = SCHEMA_DIRECTORY / "im_e_state_variables.yaml"
